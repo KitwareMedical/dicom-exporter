0d0467ae0802f1021159fedc4827d9ca2ada2995
Thibault Bruyère <thibault.bruyere@kitware.com>
2023-10-11 11:44:05 +0200

    Add --resample flag and use direction matrix

--- dicomexporter/cli.py
+++ dicomexporter/cli.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument("--no-compress", action="store_true", help="Do not compress output with ZLib (VTI) or gzip (VTK.JS)")
     parser.add_argument("--convert-12-bits", action="store_true", help="Converts from 16 to 12 bits")
     parser.add_argument("--overwrite", action="store_true", help="Overwrite output")
+    parser.add_argument("--resample", action="store_true", help="Resample using the closest axis aligned orientation, discard volume orientation")
 
     args = parser.parse_args()
 
@@ -19,4 +20,5 @@ def main():
         overwrite=args.overwrite,
         compress=not args.no_compress,
         convert_12_bits=args.convert_12_bits,
+        resample=args.resample,
     )
